c8b67599c86536abfa373020cefa91b1c1155c1c
Dan Drew <dan.drew@hotmail.com>
2024-07-19 10:04:16 -0700

    FIX: Crash when selecting a H2H game (eg. Playground Insults) and then hitting reset (trash) button

--- src/app/booth/guess-answers-form/guess-answers-form.component.ts
+++ src/app/booth/guess-answers-form/guess-answers-form.component.ts
@@ -42,7 +42,6 @@ export class GuessAnswersFormComponent implements OnInit, OnDestroy {
 
   reset() {
     this.form.reset()
-    this.guessing.guesses.reset()
   }
 
   private addGuess(value: string = ''): boolean {
