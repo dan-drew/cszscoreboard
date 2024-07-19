89a38fe02b47936ff2d867ab9dccb81c658b5e0d
Dan Drew <dan.drew@hotmail.com>
2024-07-19 10:07:48 -0700

    FIX: Crash when selecting a H2H game (eg. Playground Insults) and then hitting reset (trash) button * Build and deploy

--- src/app/build-info.ts
+++ src/app/build-info.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 
-const BUILD_TIME = "2024-07-02T09:39:35-07:00"
+const BUILD_TIME = "2024-07-19T10:06:40-07:00"
 
 @Injectable({providedIn: "root"})
 export class BuildInfo {
