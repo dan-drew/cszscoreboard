418a635ff61a6f4c9f160857626adca20dc587c4
Dan Drew <dan.drew@hotmail.com>
2026-07-19 22:59:31 -0700

    update deploy version

--- src/app/build-info.ts
+++ src/app/build-info.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 
-const BUILD_TIME = "2026-07-19T22:51:42-07:00"
+const BUILD_TIME = "2026-07-19T22:54:26-07:00"
 
 @Injectable({providedIn: "root"})
 export class BuildInfo {
